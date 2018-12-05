28f5fa287c27a54f544d1d3034ac6a9578b4984f
jerem <jeremy.tellaa@tanker.io>
2018-12-05 17:44:08 +0100

    add spark chip

--- starter.js
+++ starter.js
@@ -7,12 +7,15 @@ if (weapon === null) {
     weapon = getWeapon();
 }
 
-var enemy_cell = getCell(enemy);
 var pistol_cell = getCellToUseWeapon(WEAPON_PISTOL, enemy);
 var machine_cell = getCellToUseWeapon(WEAPON_MACHINE_GUN, enemy);
+var spark_cell = getCellToUseChip(CHIP_SPARK, enemy);
+var enemy_cell = getCell(enemy);
 var my_cell = getCell();
 var pistol_distance = getCellDistance(pistol_cell, my_cell);
 var machine_distance = getCellDistance(machine_cell, my_cell);
+var spark_distance = getCellDistance(spark_cell, my_cell);
+var enemy_distance = getCellDistance(enemy_cell, my_cell);
 var moves = getMP();
 
 function set_weapon(gun, current_weapon) {
@@ -36,12 +39,27 @@ function forward(cell, weapon) {
     moveTowardCell(cell);
 }
 
-if (machine_distance <= moves) {
+function use_chip(cell, chip, enemy) {
+    moveTowardCell(cell);
+    while (getTP() >= getChipCost(chip)) {
+        useChip(chip, enemy);
+    }
+    moveAwayFrom(enemy);
+}
+
+// if we are far, we can use spark and hide away
+if (enemy_distance > getChipMaxRange(CHIP_SPARK) && spark_distance <= moves) {
+    use_chip(spark_cell, CHIP_SPARK, enemy);
+// if we are close enough, let's use our strongest weapon
+} else if (machine_distance <= moves) {
     use_gun(WEAPON_MACHINE_GUN, machine_cell, weapon, enemy);
-} else if (pistol_distance < moves) {
+} else if (pistol_distance <= moves) {
     use_gun(WEAPON_PISTOL, pistol_cell, weapon, enemy);
-} else if (pistol_distance > 2 * moves) {
-    forward(pistol_cell, weapon);
+} else if (spark_distance <= moves) {
+    use_chip(spark_cell, CHIP_SPARK, enemy);
+} else if (spark_distance > 2 * moves) {
+    forward(spark_cell, weapon);
+// last minute heroic stuff
 } else if (MAX_TURNS - getTurn() < 10) {
-    forward(pistol_cell, weapon);
+    forward(spark_cell, weapon);
 }
